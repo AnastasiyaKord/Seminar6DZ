095bc273aec8225852112e9d891e36cb23edea59
Anastasiya <vipstop2303@mail.ru>
2023-06-05 13:36:11 +0300

    Задача 41

---
+++ Задача41/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит 
+// с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int TextToUser(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] ReadArray(int length)
+{
+    int[] array = new int[length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = TextToUser($"Введите {i}-ый элемент массива");
+    }
+
+    return array;
+}
+
+int CountPositiveElements(int[] array)
+{
+    int count = 0; 
+
+    for(int i = 0; i < array.Length; i++) 
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int n = TextToUser("Введите колличество элементов: "); 
+int[] myArray = ReadArray(n); 
+int sum = CountPositiveElements(myArray); 
+Console.WriteLine($"Было введено {sum} положительных элемента(-ов)");
